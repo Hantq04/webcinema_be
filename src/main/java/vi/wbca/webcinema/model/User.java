109fd1b2f7a8312aae3aa2fcf0595b3fc83a6ddd
Hantq04 <hantq04@gmail.com>
2025-02-25 20:38:41 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/model/User.java
+++ src/main/java/vi/wbca/webcinema/model/User.java
@@ -1,4 +1,9 @@
 package vi.wbca.webcinema.model;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
+    Long id;
 }
