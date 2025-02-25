a7408e79b4ecb06a3dc56fa460658eb500246000
Hantq04 <hantq04@gmail.com>
2025-02-25 21:31:48 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/model/User.java
+++ src/main/java/vi/wbca/webcinema/model/User.java
@@ -1,9 +1,40 @@
 package vi.wbca.webcinema.model;
 
-import lombok.AccessLevel;
+import jakarta.persistence.*;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column(name = "point")
+    Integer point;
+
+    @Column(name = "user_name")
+    String userName;
+
+    @Column(name = "email")
+    String email;
+
+    @Column(name = "name")
+    String name;
+
+    @Column(name = "phone_number")
+    String phoneNumber;
+
+    @Column(name = "password")
+    String password;
+
+    @Column(name = "is_active")
+    boolean isActive;
 }
