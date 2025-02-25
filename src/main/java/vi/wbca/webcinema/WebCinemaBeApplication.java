64b0b78b5565105f205f235a582d22426f6bc93b
Hantq04 <hantq04@gmail.com>
2025-02-25 20:37:25 +0700

    initial commit

---
+++ src/main/java/vi/wbca/webcinema/WebCinemaBeApplication.java
@@ -0,0 +1,13 @@
+package vi.wbca.webcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCinemaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCinemaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vi/wbca/webcinema/WebCinemaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package vi.wbca.webcinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCinemaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
