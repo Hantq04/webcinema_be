73e9291e4c7ce1c309a6eb0f3f7c14de1b083898
Hantq04 <hantq04@gmail.com>
2025-02-26 18:49:23 +0700

    initial commit

---
+++ src/main/java/vi/wbca/webcinema/repository/MovieRepo.java
@@ -0,0 +1,9 @@
+package vi.wbca.webcinema.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vi.wbca.webcinema.model.Movie;
+
+@Repository
+public interface MovieRepo extends JpaRepository<Movie, Long> {
+}
