c82f642852d755f7063cf5785a6f6868f48d61fd
Hantq04 <hantq04@gmail.com>
2025-02-28 23:32:26 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/service/userService/UserServiceImpl.java
+++ src/main/java/vi/wbca/webcinema/service/userService/UserServiceImpl.java
@@ -1,29 +1,80 @@
 package vi.wbca.webcinema.service.userService;
 
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import vi.wbca.webcinema.dto.UserDTO;
 import vi.wbca.webcinema.exception.AppException;
 import vi.wbca.webcinema.exception.ErrorCode;
 import vi.wbca.webcinema.mapper.UserMapper;
+import vi.wbca.webcinema.model.Role;
 import vi.wbca.webcinema.model.User;
+import vi.wbca.webcinema.repository.RoleRepo;
 import vi.wbca.webcinema.repository.UserRepo;
+import vi.wbca.webcinema.util.Informations;
+import vi.wbca.webcinema.util.jwt.JwtTokenProvider;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService{
-    private final UserRepo userRepo;
-    private final UserMapper userMapper;
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserServiceImpl implements UserService {
+    UserRepo userRepo;
+    UserMapper userMapper;
+    RoleRepo roleRepo;
+    PasswordEncoder passwordEncoder;
+    AuthenticationManager authenticationManager;
 
     @Override
-    public User insertUser(UserDTO request) {
-        User newUser = userMapper.toUser(request);
-        if (userRepo.existsByUserName(newUser.getUserName())) {
+    public void register(UserDTO request) {
+        if (userRepo.existsByUserName(request.getUserName())) {
             throw new AppException(ErrorCode.USERNAME_EXISTED);
         }
-        return userRepo.save(newUser);
+        User user = userMapper.toUser(request);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        userRepo.save(user);
+        request.getListRoles().forEach(role -> addRole(role, user));
+    }
+
+    public void addRole(String role, User user) {
+        switch (role) {
+            case Informations.ADMIN -> roleRepo.save(Role.builder()
+                    .roleName(Informations.ROLE_ADMIN_NAME)
+                    .code(Informations.ADMIN)
+                    .user(user).build());
+            case Informations.USER -> roleRepo.save(Role.builder()
+                    .roleName(Informations.ROLE_USER_NAME)
+                    .code(Informations.USER)
+                    .user(user).build());
+            default -> throw new AppException(ErrorCode.INVALID_ROLE);
+        }
+    }
+
+    @Override
+    public UserDTO login(UserDTO userDTO) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        userDTO.getUserName(),
+                        userDTO.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String jwt = JwtTokenProvider.generateToken(userDetails);
+        User user = userRepo.findByUserName(userDetails.getUsername())
+                .orElseThrow(() -> new  AppException(ErrorCode.USERNAME_NOT_FOUND));
+        UserDTO response = userMapper.toUserDTO(user);
+        response.setToken(jwt);
+        return response;
     }
 
     @Override
