da124df1121613cec5f1246d4c27a3f59f25f338
Hantq04 <hantq04@gmail.com>
2025-02-28 23:32:26 +0700

    initial commit

---
+++ src/main/java/vi/wbca/webcinema/groupValidate/LoginUser.java
@@ -0,0 +1,4 @@
+package vi.wbca.webcinema.groupValidate;
+
+public class LoginUser {
+}
--- src/main/java/vi/wbca/webcinema/repository/RoleRepo.java
+++ src/main/java/vi/wbca/webcinema/repository/RoleRepo.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vi.wbca.webcinema.model.Role;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepo extends JpaRepository<Role, Long> {
+    Optional<Role> findByCode(String code);
 }
