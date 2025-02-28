d458004b8374fa59f8d62d3ce3f112fa7a5f3398
Hantq04 <hantq04@gmail.com>
2025-02-28 19:33:15 +0700

    feat: add logic to jwt auth and web secu config

--- src/main/java/vi/wbca/webcinema/config/JwtAuthenticationFilter.java
+++ src/main/java/vi/wbca/webcinema/config/JwtAuthenticationFilter.java
@@ -1,13 +1,21 @@
 package vi.wbca.webcinema.config;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AccessLevel;
+import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
@@ -36,7 +44,33 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authToken = request.getHeader("Authorization");
+        try {
+            if (authToken == null || !authToken.startsWith("Bearer ")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            String jwt = authToken.substring(7);
+            String userName = jwtTokenProvider.extractUserName(jwt);
+            if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userName);
+                if (jwtTokenProvider.isTokenValid(jwt, userDetails)) {
+                    UsernamePasswordAuthenticationToken authentication =
+                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                } else if (userRepo.findByEmail(userDetails.getUsername()).isPresent()) {
+                    throw new ExpiredJwtException(null, null, "JWT token is expired.");
+                }
+            }
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException | SignatureException | UsernameNotFoundException exception) {
+            exceptionResolver.resolveException(request, response, null, exception);
+        }
     }
 }
