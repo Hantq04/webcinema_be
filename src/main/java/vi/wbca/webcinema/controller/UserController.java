3419fd37dab89fa27f6ed46287ecfa9ebb56494e
Hantq04 <hantq04@gmail.com>
2025-02-28 23:32:26 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/controller/UserController.java
+++ src/main/java/vi/wbca/webcinema/controller/UserController.java
@@ -8,21 +8,44 @@ import org.springframework.web.bind.annotation.*;
 import vi.wbca.webcinema.dto.UserDTO;
 import vi.wbca.webcinema.model.User;
 import vi.wbca.webcinema.service.userService.UserService;
+import vi.wbca.webcinema.util.Informations;
 import vi.wbca.webcinema.util.response.ResponseObject;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
 public class UserController {
+    private final Logger logger = Logger.getLogger(UserController.class.getName());
     private final UserService userService;
 
-    @PostMapping("/insert")
-    public ResponseEntity<ResponseObject> insertUser(@Valid @RequestBody UserDTO request) {
-        User responseData = userService.insertUser(request);
+    @PostMapping("/register")
+    public ResponseEntity<ResponseObject> register(@Valid @RequestBody UserDTO request) {
+
+        logger.info("----------Web Cinema: Insert New User----------");
+
+        userService.register(request);
+
+        Map<String, String> responseData = new HashMap<>();
+        responseData.put(Informations.USER_NAME, request.getUserName());
+        responseData.put(Informations.EMAIL, request.getEmail());
+        responseData.put(Informations.LIST_ROLE, request.getListRoles().toString());
+
         return ResponseEntity.status(HttpStatus.OK).body(
-                new ResponseObject(HttpStatus.OK, "User insert successfully", responseData)
+                new ResponseObject(HttpStatus.OK, "User insert successfully.", responseData)
+        );
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ResponseObject> login(@Valid @RequestBody UserDTO userDTO) {
+        logger.info("----------Web Cinema: Login Page----------");
+        UserDTO responseData = userService.login(userDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject(HttpStatus.OK, "User login successfully.", responseData)
         );
     }
 
---
+++ src/main/java/vi/wbca/webcinema/util/Informations.java
@@ -0,0 +1,17 @@
+package vi.wbca.webcinema.util;
+
+public class Informations {
+
+    // Role code
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+
+    // Role name
+    public static final String ROLE_ADMIN_NAME = "Administrator";
+    public static final String ROLE_USER_NAME = "Regular user";
+
+    // Information
+    public static final String USER_NAME = "userName";
+    public static final String EMAIL = "email";
+    public static final String LIST_ROLE = "listRole";
+}
