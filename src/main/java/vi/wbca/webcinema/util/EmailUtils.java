c63dcc59923c1708d42d785c3d9765c15c192f70
Hantq04 <hantq04@gmail.com>
2025-03-02 16:41:46 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/util/EmailUtils.java
+++ src/main/java/vi/wbca/webcinema/util/EmailUtils.java
@@ -17,10 +17,21 @@ public class EmailUtils {
 
     public static String getResendEmailMessage(String otp) {
         return String.format(
-                        "<p>You have requested to resend your One-Time Password (OTP) for verification.</p>" +
+                "<p>You have requested to resend your One-Time Password (OTP) for verification.</p>" +
                         "<p>Your new OTP is:</p>" +
                         "<h2>%s</h2>" +
                         "<p>Please enter this OTP to verify your account. This OTP is valid for a limited time.</p>" +
+                        "<p>Thank you <br> Support Team</p>",
+                otp
+        );
+    }
+
+    public static String getChangePasswordMessage(String otp) {
+        return String.format(
+                "<p>You have requested to change your password.</p>" +
+                        "<p>Your One-Time Password (OTP) for changing your password is:</p>" +
+                        "<h2>%s</h2>" +
+                        "<p>Please enter this OTP to proceed with changing your password. This OTP is valid for a limited time.</p>" +
                         "<p>Thank you <br> Support Team</p>",
                 otp
         );
