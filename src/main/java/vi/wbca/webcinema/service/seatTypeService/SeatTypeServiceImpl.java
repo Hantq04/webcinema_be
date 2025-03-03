f8af4d750c98f810dd47d89a2beef54f7a1f3717
Hantq04 <hantq04@gmail.com>
2025-03-03 18:43:48 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/service/seatTypeService/SeatTypeServiceImpl.java
+++ src/main/java/vi/wbca/webcinema/service/seatTypeService/SeatTypeServiceImpl.java
@@ -1,4 +1,24 @@
 package vi.wbca.webcinema.service.seatTypeService;
 
-public class SeatTypeServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import vi.wbca.webcinema.model.SeatType;
+import vi.wbca.webcinema.repository.SeatTypeRepo;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SeatTypeServiceImpl implements SeatTypeService{
+    private final SeatTypeRepo seatTypeRepo;
+
+    @Override
+    public SeatType insertSeatType(SeatType seatType) {
+        return seatTypeRepo.save(seatType);
+    }
+
+    @Override
+    public List<SeatType> getAllType() {
+        return seatTypeRepo.findAll();
+    }
 }
