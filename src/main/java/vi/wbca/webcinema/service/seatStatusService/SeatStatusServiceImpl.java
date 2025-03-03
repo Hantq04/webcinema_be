66d0667176177bd42cafa92906589f5c56a3aa41
Hantq04 <hantq04@gmail.com>
2025-03-03 17:51:14 +0700

    initial commit

---
+++ src/main/java/vi/wbca/webcinema/service/seatStatusService/SeatStatusServiceImpl.java
@@ -0,0 +1,24 @@
+package vi.wbca.webcinema.service.seatStatusService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import vi.wbca.webcinema.model.SeatStatus;
+import vi.wbca.webcinema.repository.SeatStatusRepo;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SeatStatusServiceImpl implements SeatStatusService {
+    private final SeatStatusRepo seatStatusRepo;
+
+    @Override
+    public SeatStatus insertSeatStatus(SeatStatus seatStatus) {
+        return seatStatusRepo.save(seatStatus);
+    }
+
+    @Override
+    public List<SeatStatus> getAllStatus() {
+        return seatStatusRepo.findAll();
+    }
+}
