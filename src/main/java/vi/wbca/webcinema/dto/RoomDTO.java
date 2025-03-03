d69f43779438f7914d97bab93ba10334d14333a9
Hantq04 <hantq04@gmail.com>
2025-03-03 11:14:57 +0700

    initial commit

---
+++ src/main/java/vi/wbca/webcinema/dto/RoomDTO.java
@@ -0,0 +1,39 @@
+package vi.wbca.webcinema.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import vi.wbca.webcinema.enums.RoomType;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RoomDTO {
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    Long id;
+
+    @NotNull(message = "NOT_NULL")
+    @Min(value = 50, message = "INVALID_CAPACITY")
+    @Max(value = 400, message = "INVALID_CAPACITY")
+    Integer capacity;
+
+    @Enumerated(EnumType.STRING)
+    RoomType type;
+
+    @NotBlank(message = "NOT_BLANK")
+    @Size(min = 6, max = 50, message = "INVALID_DESCRIPTION")
+    String description;
+
+    @NotBlank(message = "NOT_BLANK")
+    @Size(min = 2, max = 5, message = "INVALID_CODE")
+    String code;
+
+    @NotBlank(message = "NOT_BLANK")
+    @Size(min = 3, max = 20, message = "INVALID_NAME")
+    String name;
+}
