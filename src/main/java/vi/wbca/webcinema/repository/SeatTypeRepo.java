0025b9e8a4af767c47a15707f3842234953d7aed
Hantq04 <hantq04@gmail.com>
2025-03-03 18:43:48 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/repository/SeatTypeRepo.java
+++ src/main/java/vi/wbca/webcinema/repository/SeatTypeRepo.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vi.wbca.webcinema.model.SeatType;
 
+import java.util.Optional;
+
 @Repository
 public interface SeatTypeRepo extends JpaRepository<SeatType, Long> {
+    Optional<SeatType> findByNameType(String name);
 }
