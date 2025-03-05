8bd8b2f4fcfa70fe4906a0d771569b3a8dde80c4
Hantq04 <hantq04@gmail.com>
2025-03-05 11:47:09 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/repository/AccessTokenRepo.java
+++ src/main/java/vi/wbca/webcinema/repository/AccessTokenRepo.java
@@ -2,11 +2,16 @@ package vi.wbca.webcinema.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import vi.wbca.webcinema.enums.TokenStatus;
 import vi.wbca.webcinema.model.AccessToken;
+import vi.wbca.webcinema.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AccessTokenRepo extends JpaRepository<AccessToken, Long> {
     Optional<AccessToken> findByAccessToken(String token);
+
+    List<AccessToken> findAllByUserAndTokenStatus(User user, TokenStatus active);
 }
