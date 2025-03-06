d8b0c99e2b952eec54a75f2ca675de2f205dfc19
Hantq04 <hantq04@gmail.com>
2025-03-06 10:43:05 +0700

    initial commit

---
+++ src/main/java/vi/wbca/webcinema/dto/MovieDTO.java
@@ -0,0 +1,69 @@
+package vi.wbca.webcinema.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.validator.constraints.URL;
+import vi.wbca.webcinema.repository.MovieTypeRepo;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class MovieDTO {
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    Long id;
+
+    @NotNull(message = "NOT_NULL")
+    Integer movieDuration;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Ho_Chi_Minh")
+    Date endTime;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Ho_Chi_Minh")
+    Date premiereDate;
+
+    @NotBlank(message = "NOT_BLANK")
+    @Size(min = 6, max = 50, message = "INVALID_DESCRIPTION")
+    String description;
+
+    @NotBlank(message = "NOT_BLANK")
+    @Size(min = 3, max = 20, message = "INVALID_DIRECTOR")
+    String director;
+
+    @NotBlank(message = "NOT_BLANK")
+    @Pattern(
+            regexp = "^(?:[a-zA-Z]:\\\\|/)?(?:[^\\/:*?\"<>|]+[/\\\\])*[^\\/:*?\"<>|]+\\.(jpg|jpeg|png|gif|bmp)$",
+            message = "INVALID_IMAGE_PATH"
+    )
+    String image;
+
+    @NotBlank(message = "NOT_BLANK")
+    @Pattern(
+            regexp = "^(?:[a-zA-Z]:\\\\|/)?(?:[^\\/:*?\"<>|]+[/\\\\])*[^\\/:*?\"<>|]+\\.(jpg|jpeg|png|gif|bmp)$",
+            message = "INVALID_IMAGE_PATH"
+    )
+    String heroImage;
+
+    @NotBlank(message = "NOT_BLANK")
+    @Size(min = 3, max = 20, message = "INVALID_LANGUAGE")
+    String language;
+
+    @NotBlank(message = "NOT_BLANK")
+    @Size(min = 3, max = 20, message = "INVALID_NAME")
+    String name;
+
+    @NotBlank(message = "NOT_BLANK")
+    @URL(message = "INVALID_TRAILER_FORM")
+    String trailer;
+
+    String movieTypeName;
+}
