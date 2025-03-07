9c8e690530b6f6f4c071fe1e4459cf1561e0ae30
Hantq04 <hantq04@gmail.com>
2025-03-07 23:01:41 +0700

    initial commit

---
+++ src/main/java/vi/wbca/webcinema/dto/BillDTO.java
@@ -0,0 +1,36 @@
+package vi.wbca.webcinema.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class BillDTO {
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    Long id;
+
+    Double totalMoney;
+
+    String tradingCode;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Ho_Chi_Minh")
+    Date createTime;
+
+    String name;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Ho_Chi_Minh")
+    Date updateTime;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    String customerName;
+}
