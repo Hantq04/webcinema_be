90e1f55226a1aeb3cf8b8c04e95b1b54e3b7bad2
Hantq04 <hantq04@gmail.com>
2025-03-12 22:12:15 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/repository/SeatRepo.java
+++ src/main/java/vi/wbca/webcinema/repository/SeatRepo.java
@@ -2,12 +2,16 @@ package vi.wbca.webcinema.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import vi.wbca.webcinema.enums.ESeatStatus;
 import vi.wbca.webcinema.model.Room;
 import vi.wbca.webcinema.model.Seat;
+import vi.wbca.webcinema.model.SeatStatus;
 
 import java.util.Optional;
 
 @Repository
 public interface SeatRepo extends JpaRepository<Seat, Long> {
     Optional<Seat> findByLineAndNumberAndRoom(String line, Integer number, Room room);
+
+    Integer countByRoom(Room room);
 }
