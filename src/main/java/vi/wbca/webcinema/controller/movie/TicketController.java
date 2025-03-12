bc160857cfe593c13911a63a676ca5da61622dee
Hantq04 <hantq04@gmail.com>
2025-03-12 17:29:56 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/controller/movie/TicketController.java
+++ src/main/java/vi/wbca/webcinema/controller/movie/TicketController.java
@@ -1,11 +1,44 @@
 package vi.wbca.webcinema.controller.movie;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import vi.wbca.webcinema.dto.TicketDTO;
+import vi.wbca.webcinema.service.ticketService.TicketService;
+import vi.wbca.webcinema.util.response.ResponseObject;
+
+import java.util.logging.Logger;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/ticket")
 public class TicketController {
+    private static final Logger logger = Logger.getLogger(TicketController.class.getName());
+    private final TicketService ticketService;
+
+    @PostMapping("/insert")
+    public ResponseEntity<ResponseObject> insertTicket(@Valid @RequestBody TicketDTO request) {
+
+        logger.info("----------Web Cinema: Insert New Ticket----------");
+
+        ticketService.insertTicket(request);
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject(HttpStatus.OK, "Insert ticket successfully.", request)
+        );
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseObject> deleteTicket(@Valid @RequestParam String code) {
+
+        logger.info("----------Web Cinema: Delete Ticket----------");
+
+        ticketService.deleteTicket(code);
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject(HttpStatus.OK, "Deleted ticket successfully.", "")
+        );
+    }
 }
--- src/main/java/vi/wbca/webcinema/repository/SeatRepo.java
+++ src/main/java/vi/wbca/webcinema/repository/SeatRepo.java
@@ -2,11 +2,12 @@ package vi.wbca.webcinema.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import vi.wbca.webcinema.model.Room;
 import vi.wbca.webcinema.model.Seat;
 
 import java.util.Optional;
 
 @Repository
 public interface SeatRepo extends JpaRepository<Seat, Long> {
-    Optional<Seat> findByLineAndNumber(String line, Integer number);
+    Optional<Seat> findByLineAndNumberAndRoom(String line, Integer number, Room room);
 }
