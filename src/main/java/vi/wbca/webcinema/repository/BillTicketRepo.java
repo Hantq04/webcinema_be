8399e04e81910442c4cf0d74c84837f7961fbd5a
Hantq04 <hantq04@gmail.com>
2025-03-13 15:14:48 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/repository/BillTicketRepo.java
+++ src/main/java/vi/wbca/webcinema/repository/BillTicketRepo.java
@@ -1,9 +1,14 @@
 package vi.wbca.webcinema.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import vi.wbca.webcinema.model.BillTicket;
 
+import java.util.List;
+
 @Repository
 public interface BillTicketRepo extends JpaRepository<BillTicket, Long> {
+    @Query("SELECT bt FROM BillTicket bt WHERE bt.bill.id = :billId")
+    List<BillTicket> findAllByBillId(Long billId);
 }
