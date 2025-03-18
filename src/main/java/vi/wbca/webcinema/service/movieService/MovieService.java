0b26ab6d7fb05895df8433f58ecfdfde5e29e735
Hantq04 <hantq04@gmail.com>
2025-03-18 22:58:20 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/service/movieService/MovieService.java
+++ src/main/java/vi/wbca/webcinema/service/movieService/MovieService.java
@@ -1,9 +1,15 @@
 package vi.wbca.webcinema.service.movieService;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import vi.wbca.webcinema.dto.MovieDTO;
 
 public interface MovieService {
     MovieDTO insertMovie(MovieDTO movieDTO);
+
     void updateMovie(MovieDTO movieDTO);
+
     void deleteMovie(String name);
+
+    Page<MovieDTO> getMoviePage(Pageable pageable);
 }
