d5513579dbd26890057e1e72185f0e68a5d103fc
Hantq04 <hantq04@gmail.com>
2025-03-20 11:50:18 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/repository/MovieRepo.java
+++ src/main/java/vi/wbca/webcinema/repository/MovieRepo.java
@@ -1,7 +1,10 @@
 package vi.wbca.webcinema.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import vi.wbca.webcinema.dto.MovieDTO;
 import vi.wbca.webcinema.model.Movie;
 
 import java.util.Optional;
@@ -9,4 +12,6 @@ import java.util.Optional;
 @Repository
 public interface MovieRepo extends JpaRepository<Movie, Long> {
     Optional<Movie> findByName(String name);
+
+    Page<MovieDTO> getTicketStatistics(Pageable pageable);
 }
