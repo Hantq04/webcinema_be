b9021fd46b6a13b519fb1b203c47197d61b9a883
Hantq04 <hantq04@gmail.com>
2025-03-22 23:43:50 +0700

    initial commit

---
+++ src/main/java/vi/wbca/webcinema/dto/cinema/FoodRevenueDTO.java
@@ -0,0 +1,15 @@
+package vi.wbca.webcinema.dto.cinema;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class FoodRevenueDTO {
+    String nameOfFood;
+
+    Double totalRevenue;
+}
