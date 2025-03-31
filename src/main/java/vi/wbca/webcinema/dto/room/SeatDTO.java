902f385bddaedae4846aa0133a75fdfe0a20e747
Hantq04 <hantq04@gmail.com>
2025-03-31 10:30:15 +0700

    feat: add group validate for seat

--- src/main/java/vi/wbca/webcinema/dto/room/SeatDTO.java
+++ src/main/java/vi/wbca/webcinema/dto/room/SeatDTO.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import vi.wbca.webcinema.groupValidate.seat.InsertSeat;
+import vi.wbca.webcinema.groupValidate.seat.UpdateSeat;
 
 @Getter
 @Setter
@@ -14,18 +16,18 @@ public class SeatDTO {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     Long id;
 
-    @NotNull(message = "NOT_NULL")
-    @Min(value = 1, message = "INVALID_NUMBER_SEAT")
-    @Max(value = 20, message = "INVALID_NUMBER_SEAT")
+    @NotNull(message = "NOT_NULL", groups = {UpdateSeat.class})
+    @Min(value = 1, message = "INVALID_NUMBER_SEAT", groups = {UpdateSeat.class})
+    @Max(value = 20, message = "INVALID_NUMBER_SEAT", groups = {UpdateSeat.class})
     Integer number;
 
-    @NotBlank(message = "NOT_BLANK")
-    @Pattern(regexp = "^[A-Z]$", message = "INVALID_LINE")
+    @NotBlank(message = "NOT_BLANK", groups = {UpdateSeat.class})
+    @Pattern(regexp = "^[A-Z]$", message = "INVALID_LINE", groups = {UpdateSeat.class})
     String line;
 
-    @NotBlank(message = "NOT_BLANK")
+    @NotBlank(message = "NOT_BLANK", groups = {InsertSeat.class, UpdateSeat.class})
     String roomName;
 
-    @NotBlank(message = "NOT_BLANK")
+    @NotBlank(message = "NOT_BLANK", groups = {InsertSeat.class, UpdateSeat.class})
     String roomCode;
 }
