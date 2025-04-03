108527f4542a95866c4088d18b01a813992fd50d
Hantq04 <hantq04@gmail.com>
2025-04-03 09:19:17 +0700

    initial commit

---
+++ src/main/resources/database/data.sql
@@ -0,0 +1,16 @@
+-- Insert all values into the database tables
+INSERT INTO movie_types (is_active, movie_type_name) VALUES
+(true, 'Action'),
+(true, 'Adventure'),
+(true, 'Animation'),
+(true, 'Comedy'),
+(true, 'Crime'),
+(true, 'Documentary'),
+(true, 'Drama'),
+(true, 'Fantasy'),
+(true, 'Horror'),
+(true, 'Mystery'),
+(true, 'Romance'),
+(true, 'Sci-fi'),
+(true, 'Thriller'),
+(true, 'War');
