3f3328595d2c360d9af80dda74496d3d43b5537b
Hantq04 <hantq04@gmail.com>
2025-04-04 22:21:17 +0700

    fix: modify data in data.sql

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -1,16 +1,16 @@
 -- Insert all values into the database tables
 
 INSERT INTO banners (image_url, title) VALUES
-    ('src/home/image/movie_image.jpg', 'Thunderbolt* - Coming soon');
+    ('src/home/image/movie_image.jpg', 'Thunderbolt* - Sắp ra mắt');
 
 INSERT INTO foods (description, image, is_active, name_of_food, price) VALUES
-    ('food description', 'src/image/food_image.jpg', true, 'Bỏng ngô', 30000),
-    ('food description', 'src/image/food_image.jpg', true, 'Coca cola', 20000),
-    ('food description', 'src/image/food_image.jpg', true, 'Pepsi', 20000),
-    ('food description', 'src/image/food_image.jpg', true, 'Fanta', 20000),
-    ('food description', 'src/image/food_image.jpg', true, 'Sprite', 20000),
-    ('food description', 'src/image/food_image.jpg', true, 'Combo 2 người', 120000),
-    ('food description', 'src/image/food_image.jpg', true, 'Combo 3 người', 185000);
+    ('mô tả đồ ăn', 'src/image/food_image.jpg', true, 'Bỏng ngô', 30000),
+    ('mô tả đồ ăn', 'src/image/food_image.jpg', true, 'Coca cola', 20000),
+    ('mô tả đồ ăn', 'src/image/food_image.jpg', true, 'Pepsi', 20000),
+    ('mô tả đồ ăn', 'src/image/food_image.jpg', true, 'Fanta', 20000),
+    ('mô tả đồ ăn', 'src/image/food_image.jpg', true, 'Sprite', 20000),
+    ('mô tả đồ ăn', 'src/image/food_image.jpg', true, 'Combo 2 người', 120000),
+    ('mô tả đồ ăn', 'src/image/food_image.jpg', true, 'Combo 3 người', 185000);
 
 INSERT INTO general_settings (break_time, business_hours, close_time, percent_weekend, time_begin_to_change, open_time) VALUES
     ('12:00:00', 18, '02:00:00', 20, '2025-04-15 00:00:00', '08:00:00');
