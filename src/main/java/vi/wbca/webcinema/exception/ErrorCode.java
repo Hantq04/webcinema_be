a4f90b35c75061be610414cf080aaf9cdaae6c5e
Hantq04 <hantq04@gmail.com>
2025-04-10 10:24:35 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/exception/ErrorCode.java
+++ src/main/java/vi/wbca/webcinema/exception/ErrorCode.java
@@ -12,9 +12,9 @@ public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error!", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_KEY(1100, "Invalid message key.", HttpStatus.BAD_REQUEST),
     UNAUTHENTICATED(1101, "Unauthenticated!", HttpStatus.UNAUTHORIZED),
-    UNAUTHORIZED(1102, "You don't have permission.", HttpStatus.FORBIDDEN),
+    UNAUTHORIZED(1102, "error.1102", HttpStatus.FORBIDDEN),
     INVALID_SIGNATURE(1103, "Invalid JWT signature.", HttpStatus.FORBIDDEN),
-    EXPIRED_TOKEN(1104, "JWT token is already expired.", HttpStatus.FORBIDDEN),
+    EXPIRED_TOKEN(1104, "error.1104", HttpStatus.FORBIDDEN),
     EXPIRED_REFRESH_TOKEN(1105, "Refresh token is already expired. Please login again.", HttpStatus.FORBIDDEN),
 
     // Throw Exception, code 12**
