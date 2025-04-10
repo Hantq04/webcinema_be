b2c74e8a04604e7a99d89325308680911c9dfcee
Hantq04 <hantq04@gmail.com>
2025-04-10 10:01:53 +0700

    initial commit

--- src/main/java/vi/wbca/webcinema/util/MessageUtils.java
+++ src/main/java/vi/wbca/webcinema/util/MessageUtils.java
@@ -1,4 +1,17 @@
 package vi.wbca.webcinema.util;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class MessageUtils {
+    private final MessageSource messageSource;
+
+    public String getMessage(String code) {
+        return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
+    }
+
 }
