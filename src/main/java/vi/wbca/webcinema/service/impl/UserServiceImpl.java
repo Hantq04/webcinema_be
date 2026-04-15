98b260243ae4b601a8f3f80c082abc5a3c1f0518
hantq04 <hantq04@gmail.com>
2026-04-15 11:17:52 +0700

    fix: modify spring application name

--- src/main/java/vi/wbca/webcinema/service/impl/UserServiceImpl.java
+++ src/main/java/vi/wbca/webcinema/service/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import jakarta.mail.MessagingException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.context.MessageSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
